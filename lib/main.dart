4d5daab0dbb81fc955c998a4bc8fe375dc67477a
hryktkuch <hightraffic@gmail.com>
2024-05-13 08:53:50 +0900

    レイアウト検討

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,42 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'janken',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text('じゃんけん'),
+        ),
+        body: Column(
+          //mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            // デフォルト表示
+            Text('Default'),
+            // 太さを指定
+            Text('Bold', style: TextStyle(fontWeight: FontWeight.bold)),
+            // スタイルを指定
+            OutlinedButton(
+              onPressed: () {},
+              child: Text('Italic', style: TextStyle(fontStyle: FontStyle.italic)),
+            ),
+            // サイズを指定
+            Text('fontSize = 36', style: TextStyle(fontSize: 36)),
+            // 色を指定
+            Text('Red', style: TextStyle(color: Colors.red)),
+            Container(
+              width: double.infinity,
+              color: Colors.grey,
+              // 表示位置を指定
+              child: Text('TextAlign.right', textAlign: TextAlign.right),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
         ),
       ),
     );
